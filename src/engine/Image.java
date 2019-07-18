8100712302c65a87d793cc7f45324c46a7f5ff12
Florian Schieder <florian@olfibits.de>
2019-07-18 22:31:17 +0200

    Bugfix: Grafik verzerrt

--- src/engine/Image.java
+++ src/engine/Image.java
@@ -1,10 +1,5 @@
 package engine;
 
-import java.awt.*;
-import java.awt.image.*;
-
-import javax.imageio.ImageIO;
-
 import ea.*;
 
 /**
@@ -32,21 +27,31 @@ public class Image extends Bild {
 		);
 	}
 	
-	public static BufferedImage prepareImage(String bezeichner, int width, int height) {
+	public static java.awt.image.BufferedImage prepareImage(String bezeichner, int width, int height) {
 		try {
-			BufferedImage img = (BufferedImage) ImageIO.read(Image.class.getResourceAsStream("/assets/images/" + bezeichner + ".png"));
-			GraphicsEnvironment env = GraphicsEnvironment.getLocalGraphicsEnvironment();
-			GraphicsConfiguration gc = env.getDefaultScreenDevice().getDefaultConfiguration();
+			java.awt.image.BufferedImage img = (java.awt.image.BufferedImage) javax.imageio.ImageIO.read(Image.class.getResourceAsStream("/assets/images/" + bezeichner + ".png"));
+			java.awt.GraphicsEnvironment env = java.awt.GraphicsEnvironment.getLocalGraphicsEnvironment();
+			java.awt.GraphicsConfiguration gc = env.getDefaultScreenDevice().getDefaultConfiguration();
 
-			BufferedImage resize = gc.createCompatibleImage(width, height, img.getTransparency());
-			Graphics2D g = resize.createGraphics();
-
-			g.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
-			g.drawImage(img, 0, 0, width, height, 0, 0, img.getWidth(), img.getHeight(), null);
+			java.awt.image.BufferedImage resize = gc.createCompatibleImage(width, height, img.getTransparency());
+			java.awt.Graphics2D g = resize.createGraphics();
+			
+			g.setRenderingHint(java.awt.RenderingHints.KEY_INTERPOLATION, java.awt.RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+			
+			if(width > img.getWidth() || height > img.getHeight()) {
+				for(int w = 0; w <= (int)(width / img.getWidth()); w++) {
+					for(int h = 0; h <= (int)(height / img.getHeight()); h++) {
+						g.drawImage(img, w * img.getWidth(), h * img.getHeight(), (w * img.getWidth())+img.getWidth(), (h * img.getHeight())+img.getHeight(), 0, 0, img.getWidth(), img.getHeight(), null);
+					}
+				}
+			} else {
+				g.drawImage(img, 0, 0, width, height, 0, 0, img.getWidth(), img.getHeight(), null);
+			}
 			g.dispose();
 
 			return resize;
 		} catch(Exception e) {
+			e.printStackTrace();
 			return null;
 		}
 	}
